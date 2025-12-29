0e71fb78202c8feeb519a84748fd9f08adcd5452
bielng <ngunartaban@gmail.com>
2025-12-29 17:59:24 -0600

    read the dataset of world bank on the economic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib as mlb
+import matplotlib as plt
+
+df = pd.read_csv(R'"C:\Users\TAJ\Desktop\world_economic_report\WorldBank.xlsx""C:\Users\TAJ\Desktop\world_economic_report\WorldBank.xlsx"')
 
